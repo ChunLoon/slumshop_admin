93befce09475dab8eb2361bb2e9130335f92b1b4
ChunLoon <104199648+ChunLoon@users.noreply.github.com>
2022-05-15 21:21:58 +0800

    Initial commit

---
+++ lib/constants.dart
@@ -0,0 +1,6 @@
+class CONSTANTS {
+  //static String server = "http://10.19.105.149"; //office
+ // static String server = "http://192.168.0.189";   //home for login_user() in loginscreen
+
+ static String server = "http://10.31.55.235"; //uumwifi
+}
